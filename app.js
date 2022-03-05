f8492f5d3589acdc867f3c454521e3e9c2855309
Tejas Tomar <tejastomar2002@gmail.com>
2022-03-05 15:27:35 +0530

    5th commit

--- app.js
+++ app.js
@@ -28,16 +28,17 @@ function easylevel()
          btnx.style.backgroundColor=`rgb( ${red}, ${green}, ${blue} )`;
      //     console.log(btnx);
      }
-     
+     console.log(headingColor);
      $('.r-1').click(function(){
             if(this.classList[2]===guess)
             {   var back=$(`.bt-${headingColor}`).css("background-color");
-                 
+                
+                this.style.visibility="visible";
+                $('.r-1').css("visibility","visible");
+                $('.r-1').css("background-color",this.style.backgroundColor);
                 document.querySelector(".header").style.backgroundColor=back;
                 $(".box-colors").css("display","flex");
-
-                $(".r-1").css("visibility","visible");
-
+                $(".box-colors").unbind("click");
             }
 
             else{
@@ -45,66 +46,140 @@ function easylevel()
             }
      });
      
+     //   $(".new-color").click(function(){
+     //          for(let i=1;i<=3;i++)
+     //          {   const btnx=document.querySelector('.bt-'+i);
+     //              const red=Math.floor(Math.random()*255);
+     //              const green=Math.floor(Math.random()*255);
+     //              const blue=Math.floor(Math.random()*255);
+     //           if(i==headingColor)
+     //           {
+     //                document.querySelector('.colors').innerText=`rgb(${red},${green},${blue})`;
+     //           }
+     //           btnx.style.backgroundColor=`rgb( ${red}, ${green}, ${blue} )`;
+     //           //     console.log(btnx);
+     //           }
+     //           $('.r-1').click(function(){
+     //        if(this.classList[2]===guess)
+     //        {   var back=$(`.bt-${headingColor}`).css("background-color");
+                
+     //            this.style.visibility="visible";
+     //            $('.r-1').css("visibility","visible");
+     //            $('.r-1').css("background-color",this.style.backgroundColor);
+     //            document.querySelector(".header").style.backgroundColor=back;
+     //            $(".box-colors").css("display","flex");
+     //            $(".box-colors").unbind("click");
+     //        }
 
+     //        else{
+     //             $(this).css("visibility","hidden");
+     //        }
+     // });
+     //   })
+       
 }
 function hardlevel(){
      document.querySelector('.row-2').style.display='flex';
-     // select all the colors
-     $(".r-1").css("visibility","visible");
-     $(".r-2").css("visibility","visible");
-     const headingColor=Math.floor(Math.random()*6)+1;
-     const guess=`bt-${headingColor}`;
-     // console.log(allbuttons);
+     // // select all the colors
+     // $(".box-colors").css("visibility","visible");
+     // const headingColor=Math.floor(Math.random()*6)+1;
+     // const guess=`bt-${headingColor}`;
+     // // console.log(allbuttons);
+     // for(let i=1;i<=6;i++)
+     // {   const btnx=document.querySelector('.bt-'+i);
+     //     const red=Math.floor(Math.random()*255);
+     //     const green=Math.floor(Math.random()*255);
+     //     const blue=Math.floor(Math.random()*255);
+     //     if(i==headingColor)
+     //     {
+     //          document.querySelector('.colors').innerText=`rgb(${red},${green},${blue})`;
+     //     }
+     //     btnx.style.backgroundColor=`rgb( ${red}, ${green}, ${blue} )`;
+     
+     // }
+     // $('.box-colors').click(function(){
+     //        if(this.classList[2]===guess)
+     //        {   var back=$(`.bt-${headingColor}`).css("background-color");
+                 
+     //            document.querySelector(".header").style.backgroundColor=back;
+     //            $(".box-colors").css("display","flex");
+
+     //            $(".r-1,.r-2").css("visibility","visible");
+
+     //        }
+
+     //        else{
+     //             $(this).css("visibility","hidden");
+     //        }
+     // });
+     $(".box-colors").css("visibility","visible");
+     const randomNumber=Math.floor(Math.random()*6)+1;
+     const randomButton=`bt-${randomNumber}`;
+     console.log(randomNumber);
      for(let i=1;i<=6;i++)
-     {   const btnx=document.querySelector('.bt-'+i);
+     {
+         const btnx=document.querySelector('.bt-'+i);  //selecting the button
          const red=Math.floor(Math.random()*255);
          const green=Math.floor(Math.random()*255);
          const blue=Math.floor(Math.random()*255);
-         if(i==headingColor)
+         if(i===randomNumber)
          {
-              document.querySelector('.colors').innerText=`rgb(${red},${green},${blue})`;
+              document.querySelector(".colors").innerText=`rgb(${red},${green},${blue})`;
          }
-         btnx.style.backgroundColor=`rgb( ${red}, ${green}, ${blue} )`;
-     //     console.log(btnx);
+         btnx.style.backgroundColor=`rgb(${red},${green},${blue})`;
      }
-     $('.r-1').click(function(){
-            if(this.classList[2]===guess)
-            {   var back=$(`.bt-${headingColor}`).css("background-color");
-                 
-                document.querySelector(".header").style.backgroundColor=back;
-                $(".box-colors").css("display","flex");
+     $(".box-colors").click(function(){
+          if(this.classList[2]===randomButton)
+          {//  {   document.querySelector(`.bt-${randomNumber}`).style[0]="visible";
+               var back=$(`.bt-${randomNumber}`).css("background-color");
+               $(".box-colors").css("visibility","visible");
+               $(".box-colors").css("background-color",this.style.backgroundColor);
+               document.querySelector(".header").style.backgroundColor=back;
+               $(".box-colors").unbind("click");
+          }
+          else{
+               this.style.visibility="hidden";
+          }
+        
+     })
+      
+     // $(".new-color").click(function(){
+     //      for(let i=1;i<=6;i++)
+     // {
+     //     const btnx=document.querySelector('.bt-'+i);  //selecting the button
+     //     const red=Math.floor(Math.random()*255);
+     //     const green=Math.floor(Math.random()*255);
+     //     const blue=Math.floor(Math.random()*255);
+     //     if(i===randomNumber)
+     //     {
+     //          document.querySelector(".colors").innerText=`rgb(${red},${green},${blue})`;
+     //     }
+     //     btnx.style.backgroundColor=`rgb(${red},${green},${blue})`;
+     // }
+     // console.log(randomNumber);
+     // $(".box-colors").click(function(){
+     //      if(this.classList[2]===randomButton)
+     //      {//  {   document.querySelector(`.bt-${randomNumber}`).style[0]="visible";
+     //           var back=$(`.bt-${randomNumber}`).css("background-color");
+     //           $(".box-colors").css("visibility","visible");
+     //           $(".box-colors").css("background-color",this.style.backgroundColor);
+     //           document.querySelector(".header").style.backgroundColor=back;
+     //           $(".box-colors").unbind("click");
+     //      }
+     //      else{
+     //           this.style.visibility="hidden";
+     //      }
+        
+     // })
+     // })
 
-                $(".r-1,.r-2").css("visibility","visible");
-
-            }
-
-            else{
-                 $(this).css("visibility","hidden");
-            }
-     });
-     $('.r-2').click(function(){
-            if(this.classList[2]===guess)
-            {   var back=$(`.bt-${headingColor}`).css("background-color");
-                 
-                document.querySelector(".header").style.backgroundColor=back;
-                $(".box-colors").css("display","flex");
-
-                $(".r-2,.r-1").css("visibility","visible");
-
-            }
-
-            else{
-                 $(this).css("visibility","hidden");
-            }
-     });
 
 }
 function colorchangelevel()
 {   $(".r-1").css({"visibility":"visible","background-color":"white"});
     $(".r-2").css({"visibility":"visible","background-color":"white"});
     $(".box-colors").css("display","flex");
-    $(".btn-1").click(function(){
-      $(".header").css("background-color","rgba(224, 15, 8, 0.692)");
-    });
+    $(".header").css("background-color",`rgba(224, 15, 8, 0.692)`);
+    
      
 }
